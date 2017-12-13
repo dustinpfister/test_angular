c6f52adf08e9dab70faf65bb51a83e4473234735
dustinpfister <dustin.pfister@gmail.com>
2017-12-13 15:43:20 -0500

    angular-bootstrap demo

---
+++ ejs/demos/angular-bootstrap/js/index.js
@@ -0,0 +1,11 @@
+angular.module('one', []).controller('control', function($scope) {
+       $scope.mess = 'I am app one';
+});
+
+angular.module('two', []).controller('control', function($scope) {
+       $scope.mess = 'I am app two';
+});
+
+angular.bootstrap(document.getElementById('two'),['two']);
+
+console.log(angular);
