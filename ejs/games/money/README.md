81e1b8c44c0db68576fb4319c278f43af49615e1
dustinpfister <dustin.pfister@gmail.com>
2017-12-15 11:06:38 -0500

    game money work time

--- ejs/games/money/README.md
+++ ejs/games/money/README.md
@@ -3,4 +3,6 @@
 This is the first game for my test_angular project. It is a simple incremental game, that makes use of controllers, and a shared object between them made with angular.factory.
 
 
-## Get money button
+## The Work Button
+
+Click the work button to "do work" to which you get payed a small about of money each time.
--- ejs/games/money/js/index.js
+++ ejs/games/money/js/index.js
@@ -7,12 +7,21 @@ app.factory('player', function () {
         money: 0,
 
         workRate: .01, // the about of money made when clicking the work button
+        workCompleteTime: 5000, // the amount of time it takes to complete a job
+
+        working: false, // is the player currently working?
+        workingStart: false, // is the player starting to work?
+        workingStartTime: new Date, // the time the player started working
+        workingTime: 0, // the amount of time in ms the player has worked so far
+
+
         workCount: 0,
 
         moneyPerTick: .25,
+
         tickLast: new Date(),
         tickBaseRate: 1000,
-        tickRate: 1,
+        tickRate: 10
 
     };
 
@@ -27,6 +36,7 @@ app.controller('disp', function ($scope, player) {
     var setValues = function () {
 
         $scope.money = player.money;
+        $scope.workingTime = player.workingTime;
 
     };
 
@@ -43,6 +53,40 @@ app.controller('disp', function ($scope, player) {
 
             player.tickLast = now;
 
+            // if the player started working
+            if (player.workingStart) {
+
+                player.workingStart = false;
+                player.working = true;
+                player.workingStartTime = now;
+                player.workingTime = 0;
+
+            }
+
+            // i the player is working
+            if (player.working) {
+
+                player.workingTime = now - player.workingStartTime;
+
+                if (player.workingTime > player.workCompleteTime) {
+
+                    player.workingTime = player.workCompleteTime;
+
+                }
+
+                if (player.workingTime === player.workCompleteTime) {
+
+                    player.workingTime = 0;
+                    player.working = false;
+
+                    player.workCount += 1;
+                    player.money += player.workRate;
+                    player.money = Number(player.money.toFixed(2));
+
+                }
+
+            }
+
             $scope.$apply();
 
         }
@@ -58,6 +102,7 @@ app.controller('disp', function ($scope, player) {
 
 app.controller('work', function ($scope, player) {
 
+/*
     // set player values to $scope
     var setValues = function () {
 
@@ -67,14 +112,16 @@ app.controller('work', function ($scope, player) {
     };
 
     setValues();
-
+*/
     $scope.doWork = function () {
 
-        player.workCount += 1;
-        player.money += player.workRate;
-        player.money = Number(player.money.toFixed(2));
+        if (!player.workingStart && !player.working) {
 
-        setValues();
+            player.workingStart = true;
+
+        }
+
+  //      setValues();
 
     };
 
